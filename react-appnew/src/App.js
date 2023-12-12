ab5439d53a4bd1fd372a2862763d7a6842ec19c2
sivasathvik <paturisivasathvik@gmail.com>
2023-12-12 19:50:13 +0530

    Frontend App code

---
+++ react-appnew/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Login from './components/login';
+
+function App() {
+  return (
+    <div className= "App">
+      <h1>React App</h1>
+      <Login></Login>
+      
+    </div>
+  );
+}
+export default App;
